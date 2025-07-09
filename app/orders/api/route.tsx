2a5c9f8ea3d8d2b7fe7a61f0cd7fa7e0c3325eb7
KoderGoq <fernandobussines10@gmail.com>
2025-07-09 00:57:39 -0600

    dynamic api

--- app/orders/api/route.tsx
+++ app/orders/api/route.tsx
@@ -1,4 +1,6 @@
-import { prisma } from '@/src/lib/prisma'
+import { prisma } from '@/src/lib/prisma';
+
+export const dynamic = 'force-dynamic'; // This ensures the route is always fresh and not cached
 
 export const GET = async () => {
   const orders = await prisma.order.findMany({
